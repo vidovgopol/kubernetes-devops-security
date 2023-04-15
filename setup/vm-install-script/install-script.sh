b3c4df8cb6ddf4707ee625aebe1753b0029514ca
Sreenivas Dogiparthy <ds32bit@gmail.com>
2023-04-15 14:05:43 +0800

    Update Jenkins Package

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -72,11 +72,9 @@ echo ".........----------------#################._.-.-JENKINS-.-._.#############
 # wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 # sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
 
 sudo apt update
 sudo apt install -y jenkins
