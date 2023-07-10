aaeb349e7697b2a3c9a90ce9ae918766eef00dfa
Ubuntu <devsecops@devsecops-cloud.2pkqhm4c2luenlncoiayhsh5hb.bx.internal.cloudapp.net>
2023-07-10 14:25:20 +0000

    pit fix

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -36,8 +36,8 @@ public class NumericApplicationTests {
 
     @Test
     public void smallerThanOrEqualToFiftyMessage() throws Exception {
-        this.mockMvc.perform(get("/compare/49")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string("Smaller than or equal to 50"));
+        this.mockMvc.perform(get("/compare/50")).andDo(print()).andExpect(status().isOk())
+          .andExpect(content().string("Smaller than or equal to 50"));
     }
 
     @Test
@@ -45,9 +45,15 @@ public class NumericApplicationTests {
         this.mockMvc.perform(get("/compare/51")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Greater than 50"));
     }
-    
+
     @Test
     public void welcomeMessage() throws Exception {
          this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
            .andExpect(content().string("Kubernetes DevSecOps"));
-}
+	 }
+    @Test
+    public void incrementbyone() throws Exception {
+        this.mockMvc.perform(get("/increment/51")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string("52"));
+
+    }
